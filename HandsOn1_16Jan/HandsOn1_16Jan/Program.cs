b1b1ed0744ee7e0f16e16bc2a648c2cb153f61b0
Papiya Mazumder <papiyamazumder2@gmail.com>
2024-02-01 21:12:35 +0530

    Add files via upload

---
+++ HandsOn1_16Jan/HandsOn1_16Jan/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+internal class Program            // using sorted list
+{
+    private static void Main(string[] args)
+    {
+        var students = new List<Student>
+        {
+            new Student { Name = "Alice", GPA = 3.6 },
+            new Student { Name = "Bob", GPA = 3.2 },
+            new Student { Name = "Charlie", GPA = 3.8 }
+        };
+
+        SortedList<double, string> sl = new SortedList<double, string>();   // create sorted list to automatically sort 
+
+        foreach (Student s in students)             // sort automatically based on GPA
+        {
+            sl.Add(s.GPA, s.Name);
+        }
+
+        Student studentobj = new Student();         //create object of student class to call the method
+        List<String> outputlist = studentobj.FilterAndSort(sl);        // call and store the function in a list of string
+
+        outputlist.Reverse();                       // reverse for descending order
+
+        Console.WriteLine("[ " + string.Join(" , ", outputlist) + " ]");        // print in desired format
+        // OR
+        foreach (var s in outputlist)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public double GPA { get; set; }
+
+    public List<String> FilterAndSort(SortedList<double, string> sl)       // return list and take parameter sorted list
+    {
+        var filteredlist = new List<String>();
+
+        foreach (var s in sl)
+        {
+            if (s.Key > 3.5)                                // add values acc to qustn condition
+            {
+                filteredlist.Add(s.Value);
+            }
+        }
+        return filteredlist;
+    }
+
+}
+
+
---
+++ HandsOn1_16Jan/HandsOn1_16Jan/Program2.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//internal class Program2             // using Linq
+//{
+//    private static void Main(string[] args)
+//    {
+//        var students = new List<Student>
+//        {
+//            new Student { Name = "Alice", GPA = 3.6 },
+//            new Student { Name = "Bob", GPA = 3.2 },
+//            new Student { Name = "Charlie", GPA = 3.8 }
+//        };
+
+//        List<Student> result = Student.FilterAndSort(students);
+
+//        Console.WriteLine("Filtered and Sorted Students:");
+//        Console.WriteLine("[ " + string.Join(" , ", result.Select(student => student.Name)) + " ]");    // select -> Convert Student objects to string names
+//        //OR
+//        foreach (var student in result)
+//        {
+//            Console.WriteLine(student.Name);
+//        }
+//    }
+//}
+
+//public class Student
+//{
+//    public string Name { get; set; }
+//    public double GPA { get; set; }
+
+//    public static List<Student> FilterAndSort(List<Student> students)
+//    {
+//        var filteredStudents = students.Where(student => student.GPA > 3.5);        // Filter students with GPA greater than 3.5
+//        var sortedStudents = filteredStudents.OrderByDescending(student => student.GPA).ToList();       // Sort the filtered students in descending order based on GPA
+//        return sortedStudents;
+//    }
+//}
